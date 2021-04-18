01af543d3431d19ef6cdaae60e2bb5b364e41855
SHINOZAKI-RYOSUKE <gibson335m@gmail.com>
2021-04-18 10:05:04 +0000

    [update3]

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  
+  resources:books
+  resources:users
+  
+  root to: 'homes#top'
+  
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
