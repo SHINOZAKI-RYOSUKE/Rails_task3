2168e6ddf31d6e1cca1ea828e3d4a23b8625853f
SHINOZAKI-RYOSUKE <gibson335m@gmail.com>
2021-04-19 12:59:14 +0000

    [update7]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,19 @@
 class UsersController < ApplicationController
+  
   def index
     @users = User.all
+    @book = Book.new
+    @user = current_user
   end
-
+  
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @book = Book.new
   end
 
   def edit
     @user = User.find(params[:id])
   end
+  
 end
