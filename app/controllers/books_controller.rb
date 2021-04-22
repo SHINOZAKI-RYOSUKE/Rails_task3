b38435ef61889381597bd364aea559a3833d1890
SHINOZAKI-RYOSUKE <gibson335m@gmail.com>
2021-04-22 11:09:32 +0000

    [update17]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -48,16 +48,6 @@ class BooksController < ApplicationController
   end
 
 
-  def update
-    @book = Book.find(params[:id])
-    if@book.update(book_params)
-    redirect_to book_path(@book.id)
-    else
-    render "edit"
-    end
-  end
-
-
   def destroy
     book = Book.find(params[:id])
     book.destroy
