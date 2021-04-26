4f6f1c2b83589d57f79948493bff8d7118093051
SHINOZAKI-RYOSUKE <gibson335m@gmail.com>
2021-04-26 06:45:51 +0000

    [f_update2]

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources:users
   
   root to: 'homes#top'
-  get  "home/about"  => "homes#about"
+  get  "home/about"  => "homes#about", as:'about'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
